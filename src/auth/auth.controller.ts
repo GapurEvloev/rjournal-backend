ef3ffad32d2331435c7146a4f96ee4656f49a88b
gapur <kaloycant@gmail.com>
2023-01-11 23:48:31 +0100

    #10 Authentication via JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -11,6 +12,13 @@ export class AuthController {
   async login(@Request() req) {
     return this.authService.login(req.user);
   }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
   // @Post()
   // create(@Body() createAuthDto: CreateAuthDto) {
   //   return this.authService.create(createAuthDto);
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { UserEntity } from '../user/entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string): Promise<any> {
     const user = await this.userService.findByCond({
@@ -18,11 +22,16 @@ export class AuthService {
     return null;
   }
 
+  generateJwtToken(data: { id: number; email: string }) {
+    const payload = { email: data.email, sub: data.id };
+    return this.jwtService.sign(payload);
+  }
+
   async login(user: UserEntity) {
     const { password, ...userData } = user;
     return {
       ...userData,
-      // token: this.generateJwtToken(userData),
+      token: this.generateJwtToken(userData),
     };
   }
 }
